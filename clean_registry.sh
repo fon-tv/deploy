9ae792c494adf6eea1b5a72a13f162fd74a7be6c
SilentCodeSamurai <silentcodesamurai@gmail.com>
2025-04-10 14:27:10 +0300

    upd: Removed registry deploy files

--- clean_registry.sh
+++
@@ -1,1 +0,0 @@
-docker exec $(docker inspect --format '{{.Status.ContainerStatus.ContainerID}}' $(docker service ps --filter "desired-state=running" --format "{{.ID}}" "registry_registry" | head -n 1)) registry garbage-collect --delete-untagged /etc/docker/registry/config.yml
